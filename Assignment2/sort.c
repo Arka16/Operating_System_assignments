e883e8a95bb4809952799e9262b0b78ea25e4985
Arka Pal <arka.pal.0521@gmail.com>
2021-04-19 18:25:04 -0700

    Shortest working solution

--- Assignment2/sort.c
+++ Assignment2/sort.c
@@ -17,20 +17,17 @@
 #include <sys/wait.h>
 
 #define THREAD_COUNT 6
-#define POS_COUNT 8
 #define CHILD_COUNT 2
+
 
 //to store singleThreadedMergeSort params
 typedef struct arg_t{
     int *mem;
-    int L,M,R,num;
+    int L,M,R,num,t1_l, t1_r, t2_l, t2_r;
   } Arg;
 
-void * thread_create(void * args);
-void * thread_sort(void * arg);
-pthread_t thread_id[THREAD_COUNT];  //initialize all threads required to merge/sort
-Arg args_arr[THREAD_COUNT];       //store sort args in an array for loop
 
+pthread_t thread_id[THREAD_COUNT];  //initialize all threads required to merge/sort
 
 /* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
 void singleThreadedMergeSort(int arr[], int left, int right)
@@ -46,30 +43,36 @@ void singleThreadedMergeSort(int arr[], int left, int right)
 /*
  * This function stub needs to be completed
  */
+void thread_sort(void * arg){
+  Arg * args = (Arg *)arg;  //convert void* to Arg* so members can be accessed
+  singleThreadedMergeSort(args->mem, args->L, args->R);  //3,4,5,6 sort corresponding parts of arr
+}
+
+void thread_create(void * arg){
+  Arg * args = (Arg *)arg, args_arr2[2] = {{args->mem, args->t1_l, 0,args->t1_r}, {args->mem,args->t2_l,0,args->t2_r}};
+  //create thread 1 and 2 to sort both halves; thread 1 creates 3 4, thread 2 creates 5 6
+  pthread_create(&thread_id[2+args->num], NULL, (void *)&thread_sort, (void *)&args_arr2[0]);
+  pthread_create(&thread_id[3+args->num], NULL, (void *)&thread_sort, (void *)&args_arr2[1]);
+  //thread 1 waits for 3,4; thread 2 waits for 5,6
+  pthread_join(thread_id[2+args->num],NULL);
+  pthread_join(thread_id[3+args->num],NULL);
+  //thread 1 merges left half, thread 2 merges right half
+  merge(args->mem, args->L, args->M, args->R);
+}
+
 void multiThreadedMergeSort(int arr[], int left, int right)
 {
   // Your code goes here
 
   //calculate midpoint, middle of left half, and middle of right half
   int mid = (left + right)/2, quart1 = (left + mid)/2, quart3 = (right + mid)/2;
-  int Pos[POS_COUNT] = {left, quart1, mid, quart3, right, left, mid+1, left};
-
-  for(int i = 0; i < THREAD_COUNT; i++){
-    args_arr[i].mem = arr;    //store local memory
-    args_arr[i].L = i < 3 ? Pos[i+5] : Pos[i-2]+1;  //store left endpoint
-    args_arr[i].R = i < 2 ? Pos[2*i+2] : Pos[i-1]; //store right endpoint
-    args_arr[i].M = i < 2 ? Pos[2*i+1] : 0;  //store midpoint for merging
-    args_arr[i].num = 2*i;   //offest for grandchild threads
-  }
-
-  for(int i = 0; i < CHILD_COUNT; i++){
-     //Thread 1 creates 3,4, Thread 2 creates 5,6
-     pthread_create(&thread_id[i], NULL, thread_create, (void *) &args_arr[i]);
-  }
-  for(int i = 0; i < CHILD_COUNT; i++){
-    //Process waits for Thread 1 and 2
-    pthread_join(thread_id[i], NULL);
-  }
+  Arg args_arr[CHILD_COUNT] = {{arr, left, quart1, mid, 0, left, quart1, quart1+1, mid}, {arr, mid+1, quart3, right, 2, mid+1, quart3, quart3+1, right}};
+  //Thread 1 creates 3,4, Thread 2 creates 5,6
+  pthread_create(&thread_id[0], NULL, (void * )&thread_create, (void *) {&args_arr[0]});
+  pthread_create(&thread_id[1], NULL, (void * )&thread_create, (void *) {&args_arr[1]});
+  //Wait for threads to finish
+  pthread_join(thread_id[0], NULL);
+  pthread_join(thread_id[1], NULL);
   //Process merges left and right half
   merge(arr, left, mid, right);
 }
@@ -77,26 +80,8 @@ void multiThreadedMergeSort(int arr[], int left, int right)
 /*
 *HELPER FUNCTIONS
 */
-
+// {{arr, (int)left, 0,(int)quart1}, {arr, (int) quart1+1,0,(int)mid}, {{arr, (int)mid+1, 0,(int)quart3}, {arr, (int) quart3+1,0,(int) right}}
 /*For the threads to create other threads*/
-void * thread_create(void * arg){
-  Arg * args = (Arg *)arg;
-  for(int i = CHILD_COUNT; i < 4; i++){
-    //create thread 1 and 2 to sort both halves; thread 1 creates 3 4, thread 2 creates 5 6
-    pthread_create(&thread_id[args->num+i], NULL, thread_sort, (void *)&args_arr[args->num+i]);
-  }
-  for(int i = CHILD_COUNT; i < 4; i++){
-    //thread 1 waits for 3,4; thread 2 waits for 5,6
-    pthread_join(thread_id[args->num+i],NULL);
-  }
-  //thread 1 merges left half, thread 2 merges right half
-  merge(args->mem, args->L, args->M, args->R);
-  return NULL;
-}
+
 
 /*For the threads to sort array*/
-void * thread_sort(void * arg){
-  Arg * args = (Arg *)arg;  //convert void* to Arg* so members can be accessed
-  singleThreadedMergeSort(args->mem, args->L, args->R);  //3,4,5,6 sort corresponding parts of arr
-  return NULL;
-}
