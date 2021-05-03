f21a3997d70af7a5d035d726ebc9fc163736724a
Arka Pal <arka.pal.0521@gmail.com>
2021-05-02 20:00:42 -0700

    refactored code

--- Assignment3/cartman.c
+++ Assignment3/cartman.c
@@ -21,40 +21,41 @@ typedef struct arg_t{
 
 
 void * make_cart(void * param){
-    Arg* arg = param;
-    //go backwards
-    int offset = arg->J == arg->T + 1 ? -1:1;
-    //entry section
-    if(arg->C % 2 == 0){
-      //pick up left
-      sem_wait(&sems[arg->J]);
-      reserve(arg->C, arg->J);
-      //pick up right
-      sem_wait(&sems[(arg->J+ offset)%tracknum]);
-      reserve(arg->C, (arg->J + offset)%tracknum);
-      //critical section
-      cross(arg->C, arg->T, arg->J);
-      //Exit section
-      release(arg->C, arg->J);
-      sem_post(&sems[arg->J]);
-      release(arg->C, (arg->J + offset)%tracknum);
-      sem_post(&sems[(arg->J + offset)% tracknum]);
-    }
-    else{
-      //pick up right
-      sem_wait(&sems[(arg->J+ offset)%tracknum]);
-      reserve(arg->C, (arg->J + offset)%tracknum);
-      //pick up left
-      sem_wait(&sems[arg->J]);
-      reserve(arg->C, arg->J);
-      //critical section
-      cross(arg->C, arg->T, arg->J);
-      //Exit section
-      release(arg->C, (arg->J + offset)%tracknum);
-      sem_post(&sems[(arg->J + offset)% tracknum]);
-      release(arg->C, arg->J);
-      sem_post(&sems[arg->J]);
-    }
+  Arg* arg = param;
+  //go backwards or forwards
+  int offset = arg->J == arg->T + 1 ? -1:1;
+  //J1 is left if odd, right if even
+  int  J1 = arg->C % 2 != 0 ? arg->J : (arg->J + offset)%tracknum;
+  //J2 is right if odd, left if even
+  int  J2 = arg->C % 2 != 0 ? (arg->J + offset)%tracknum : arg->J;
+  if(J1 == (arg->J + offset)%tracknum && offset == -1){
+    J1 = arg->J + offset;
+  }
+  if(J1 < 0){
+    J1 = tracknum -1;
+  }
+  if(J2 < 0){
+    J2 = tracknum -1;
+  }
+  if(J1 > tracknum -1){
+    J1 = 0;
+  }
+  if(J2 > tracknum -1){
+    J2 = 0;
+  }
+  //entry section
+  sem_wait(&sems[J1]);
+  sem_wait(&sems[J2]);
+  reserve(arg->C, J1);
+  reserve(arg->C, J2);
+  //critical section
+  cross(arg->C, arg->T, arg->J);
+  //Exit section
+  release(arg->C, J1);
+  release(arg->C, J2);
+  sem_post(&sems[J1]);
+  sem_post(&sems[J2]);
+  //free memory
   free(arg);
   pthread_exit(NULL);
 }
@@ -84,7 +85,7 @@ void arrive(unsigned int cart, enum track track, enum junction junction)
 void cartman(unsigned int tracks)
 {
   tracknum = tracks; //store the total number of tracks
-  tids = malloc(sizeof(pthread_t)*tracks + 1);
+  tids = malloc(sizeof(pthread_t)*tracks);
   //initialize semaphores
   for(int i = 0; i < tracks; i++){
     sem_init(&sems[i],0,1); //create all binary semaphores
